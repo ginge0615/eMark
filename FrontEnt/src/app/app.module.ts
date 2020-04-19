1367f14261842e5aefcd18c23ce1e78b0da43d27
ginge0615 <ginge@sina.com>
2020-04-19 12:54:07 +0800

    2

--- FrontEnt/src/app/app.module.ts
+++ FrontEnt/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 
 import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/zh-Hans';
+
+//NzModule
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzI18nModule } from 'ng-zorro-antd/i18n';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+
+
+//MyCompents
 import { SignupSellerComponent } from './signup-seller/signup-seller.component';
 import { SignupBuyerComponent } from './signup-buyer/signup-buyer.component';
 import { ItemSearchComponent } from './item-search/item-search.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BuyerMainComponent } from './buyer-main/buyer-main.component';
-
-import { NzButtonModule } from 'ng-zorro-antd/button';
 
 
 @NgModule({
@@ -30,10 +43,21 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
    imports: [
       BrowserModule,
       NgbModule,
+      FormsModule,
       RouterModule.forRoot([
-         { path: '', component: BuyerMainComponent },
+         { path: '', component: SignupSellerComponent },
        ]),
-      NzButtonModule
+       NzFormModule,
+       ReactiveFormsModule,
+       NzI18nModule,
+       NzIconModule,
+       NzInputModule,
+       NzInputNumberModule,
+       NzButtonModule,
+       NzCheckboxModule,
+       NzDropDownModule,
+       NzSpaceModule,
+       NzSelectModule
    ],
    providers: [],
    bootstrap: [
--- FrontEnt/src/app/login/login.component.ts
+++ FrontEnt/src/app/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzIconService } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
+  templateUrl:'./login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  ngOnInit() {
+export class LoginComponent  implements OnInit {
+  validateForm: FormGroup;
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  constructor(private fb: FormBuilder, private iconService: NzIconService) {
+    this.iconService.fetchFromIconfont({
+      scriptUrl: 'https://at.alicdn.com/t/font_8d5l8fzk5b87iudi.js'
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
   }
 }
