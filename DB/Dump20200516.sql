bc1d4b568d3c69eb1ae0053e293fbfcf8c601c41
ginge0615 <ginge@sina.com>
2020-05-17 08:33:57 +0800

    数据库更新

--- DB/Dump20200516.sql
+++ DB/Dump20200516.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `emart-main` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `emart-main`;
 -- MySQL dump 10.13  Distrib 5.7.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: emart-main
@@ -23,16 +25,16 @@ DROP TABLE IF EXISTS `buyer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `buyer` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(30) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `mobile_phone` varchar(15) DEFAULT NULL,
-  `create_datetime` timestamp NULL DEFAULT NULL,
+  `create_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +43,7 @@ CREATE TABLE `buyer` (
 
 LOCK TABLES `buyer` WRITE;
 /*!40000 ALTER TABLE `buyer` DISABLE KEYS */;
+INSERT INTO `buyer` VALUES (1,'buyer1','123456','test@sina.com','13314285689','2020-05-16 04:03:25');
 /*!40000 ALTER TABLE `buyer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,10 +55,11 @@ DROP TABLE IF EXISTS `cart`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cart` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `buyer_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
   `number` int(11) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `cart_idx1` (`buyer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -78,11 +82,11 @@ DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(50) DEFAULT NULL,
   `brief_details` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,13 +107,14 @@ DROP TABLE IF EXISTS `discount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discount` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `discount_code` varchar(20) DEFAULT NULL,
   `percentage` decimal(3,1) DEFAULT NULL,
   `start_date` datetime DEFAULT NULL,
   `end_date` datetime DEFAULT NULL,
   `description` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `discount_code_UNIQUE` (`discount_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -131,7 +136,7 @@ DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `sell_id` int(11) DEFAULT NULL,
   `category_id` int(11) DEFAULT NULL,
   `subcategory_id` int(11) DEFAULT NULL,
@@ -162,8 +167,9 @@ DROP TABLE IF EXISTS `item_description`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item_description` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
+  `seq` int(11) DEFAULT NULL,
   `description` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -186,7 +192,7 @@ DROP TABLE IF EXISTS `item_picture`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item_picture` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `seq` int(11) DEFAULT NULL,
   `picture_path` varchar(100) DEFAULT NULL,
@@ -238,10 +244,10 @@ DROP TABLE IF EXISTS `manufactur`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `manufactur` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `manufactur_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -250,36 +256,56 @@ CREATE TABLE `manufactur` (
 
 LOCK TABLES `manufactur` WRITE;
 /*!40000 ALTER TABLE `manufactur` DISABLE KEYS */;
+INSERT INTO `manufactur` VALUES (1,'Samsung'),(2,'OPPO'),(3,'XIAOMI');
 /*!40000 ALTER TABLE `manufactur` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `purchasehistory`
+-- Table structure for table `purchase_history`
 --
 
-DROP TABLE IF EXISTS `purchasehistory`;
+DROP TABLE IF EXISTS `purchase_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `purchasehistory` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `purchase_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `buyer_id` int(11) DEFAULT NULL,
   `seller_id` int(11) DEFAULT NULL,
   `transaction_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL,
   `number` int(11) DEFAULT NULL,
   `datetime` datetime DEFAULT NULL,
+  `price` decimal(8,1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `purchasehistory`
+-- Dumping data for table `purchase_history`
 --
 
-LOCK TABLES `purchasehistory` WRITE;
-/*!40000 ALTER TABLE `purchasehistory` DISABLE KEYS */;
-/*!40000 ALTER TABLE `purchasehistory` ENABLE KEYS */;
+LOCK TABLES `purchase_history` WRITE;
+/*!40000 ALTER TABLE `purchase_history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase_history` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `report_view`
+--
+
+DROP TABLE IF EXISTS `report_view`;
+/*!50001 DROP VIEW IF EXISTS `report_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `report_view` AS SELECT 
+ 1 AS `id`,
+ 1 AS `seller_id`,
+ 1 AS `item_name`,
+ 1 AS `datetime`,
+ 1 AS `price`,
+ 1 AS `sales_volume`,
+ 1 AS `transaction_amount`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `seller`
@@ -289,7 +315,7 @@ DROP TABLE IF EXISTS `seller`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `seller` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(30) NOT NULL,
   `company_name` varchar(100) DEFAULT NULL,
@@ -299,11 +325,11 @@ CREATE TABLE `seller` (
   `website` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `contact_number` varchar(15) DEFAULT NULL,
-  `create_datetime` timestamp NULL DEFAULT NULL,
+  `create_datetime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -312,6 +338,7 @@ CREATE TABLE `seller` (
 
 LOCK TABLES `seller` WRITE;
 /*!40000 ALTER TABLE `seller` DISABLE KEYS */;
+INSERT INTO `seller` VALUES (1,'seller1','123456','ibm','gstin','test','postaddress','http://www.emart.com','test@sina.com','13314285689','2020-05-16 04:03:24');
 /*!40000 ALTER TABLE `seller` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -323,13 +350,13 @@ DROP TABLE IF EXISTS `subcategory`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subcategory` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `subcategory_name` varchar(50) DEFAULT NULL,
   `category_id` int(11) DEFAULT NULL,
   `brief_details` varchar(300) DEFAULT NULL,
   `GST` decimal(3,1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -338,6 +365,7 @@ CREATE TABLE `subcategory` (
 
 LOCK TABLES `subcategory` WRITE;
 /*!40000 ALTER TABLE `subcategory` DISABLE KEYS */;
+INSERT INTO `subcategory` VALUES (1,'Mobile',1,NULL,NULL),(2,'TV',1,NULL,NULL),(3,'MP4',1,NULL,NULL),(4,'Windbreaker',2,NULL,NULL),(5,'Skirt',2,NULL,NULL),(6,'Shoes',2,NULL,NULL),(7,'Cartoon',3,NULL,NULL),(8,'Novel',3,NULL,NULL);
 /*!40000 ALTER TABLE `subcategory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -349,7 +377,7 @@ DROP TABLE IF EXISTS `transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaction` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `buyer_id` int(11) DEFAULT NULL,
   `seller_id` int(11) DEFAULT NULL,
   `transaction_type` varchar(45) DEFAULT NULL,
@@ -369,6 +397,94 @@ LOCK TABLES `transaction` WRITE;
 UNLOCK TABLES;
 
 --
+-- Dumping routines for database 'emart-main'
+--
+/*!50003 DROP FUNCTION IF EXISTS `getCategoryName` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `GETCATEGORYNAME`(pId int) RETURNS varchar(50) CHARSET utf8
+BEGIN
+	declare categoryName varchar(50);
+    select category_name from category where id = pId into categoryName;
+RETURN categoryName;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `getCoverPicture` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `GETCOVERPICTURE`(pItemId int) RETURNS varchar(100) CHARSET utf8
+BEGIN
+	declare picturePath varchar(100);
+    select picture_path from item_picture where item_id = pItemId and seq = 1 into picturePath;
+RETURN picturePath;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `getManufacturName` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `GETMANUFACTURNAME`(pId int) RETURNS varchar(50) CHARSET utf8
+BEGIN
+	declare manufacturName varchar(50);
+    select manufactur_name from manufactur where id = pId into manufacturName;
+RETURN manufacturName;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `getSubCategoryName` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `GETSUBCATEGORYNAME`(pId int) RETURNS varchar(50) CHARSET utf8
+BEGIN
+	declare categoryName varchar(50);
+    select subcategory_name from subcategory where id = pId into categoryName;
+RETURN categoryName;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
 -- Final view structure for view `item_view`
 --
 
@@ -385,6 +501,24 @@ UNLOCK TABLES;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `report_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `report_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `report_view` AS select `i`.`id` AS `id`,`i`.`seller_id` AS `seller_id`,concat(`i`.`manufactur`,' ',`i`.`item_name`) AS `item_name`,`h`.`datetime` AS `datetime`,`i`.`price` AS `price`,`i`.`sales_volume` AS `sales_volume`,`t`.`transaction_amount` AS `transaction_amount` from ((`emart-main`.`purchase_history` `h` join `emart-main`.`transaction` `t`) join `emart-main`.`item_view` `i`) where ((`h`.`transaction_id` = `t`.`id`) and (`h`.`item_id` = `i`.`id`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -395,4 +529,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-12 23:03:14
+-- Dump completed on 2020-05-16 12:08:25
