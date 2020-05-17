02bd8f54192d2961b124fa3003d048d586b8c0ed
ginge0615 <ginge@sina.com>
2020-05-17 23:38:35 +0800

    上传图片

---
+++ MidTier/MircroService-Item/src/main/java/com/emart/model/PictureModel.java
@@ -0,0 +1,13 @@
+package com.emart.model;
+
+public class PictureModel extends MessageModel{
+	private String path;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
